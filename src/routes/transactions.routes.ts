f60eae6c3c8bdf7f90d36a19e08186c294f71c73
leonardo <leolub150@gmail.com>
2020-06-25 16:34:02 -0300

    receive transactions params on creation route

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -7,20 +7,23 @@ import { Router } from 'express';
 
 const transactionsRouter = Router();
 
-transactionsRouter.get('/', async (request, response) => {
-  // TODO
-});
+// transactionsRouter.get('/', async (request, response) => {
+//   // TODO
+// });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, type, value, category } = request.body;
+  const transaction = { title, type, value, category };
+
+  return response.json(transaction);
 });
 
-transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
-});
+// transactionsRouter.delete('/:id', async (request, response) => {
+//   // TODO
+// });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+// transactionsRouter.post('/import', async (request, response) => {
+//   // TODO
+// });
 
 export default transactionsRouter;
